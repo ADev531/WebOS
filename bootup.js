e16eba2a770cf414d827c3c3039aaa8258533eef
A Dev <82761748+ADev531@users.noreply.github.com>
2023-03-25 21:08:24 +0900

    Update bootup.js

--- bootup.js
+++ bootup.js
@@ -1,9 +1,12 @@
 function Boot() {
   try {
+    console.log("booting up...");
     InitMemManagement();
   } catch (e) {
-    document.write("Error has occoured.<br>Error Message : " + e.Message);
+    document.write("Error has occoured.<br>Error Message : " + e.Message);]
+    console.log("boot error!");
   }
 }
+
 
 Boot();
