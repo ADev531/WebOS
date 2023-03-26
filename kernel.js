2f18a01b810a9bc18e488c5752be904633acc61f
A Dev <82761748+ADev531@users.noreply.github.com>
2023-03-26 09:30:58 +0900

    Update kernel.js

--- kernel.js
+++ kernel.js
@@ -2,14 +2,20 @@ function removebioslogo() {
   document.getElementById("bootlogo").remove();
 }
 
-function addAppIcon(text) {
+function addIcon(text) {
   let icon = document.createElement("button");
   icon.innerHTML = text;
   icon.style.border = "2px solid #e7e7e7";
   document.body.appendChild(icon);
 }
 
+function useSearchBar() {
+  let search = document.createElement("input");
+  search.placeholder = "Type text to search...";
+  document.body.appendChild(search);
+}
+
 function mainKernel() {
   removebioslogo();
-  addAppIcon("Kernel Debugging..")
+  addIcon("Kernel Debugging..")
 }
