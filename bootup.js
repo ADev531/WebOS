aeb1fdb15d5ddcb71761dde9d727661cf9d17cb2
A Dev <82761748+ADev531@users.noreply.github.com>
2023-03-26 12:07:21 +0900

    Update bootup.js

--- bootup.js
+++ bootup.js
@@ -2,11 +2,19 @@ function InitMemManagement() {
   console.log("preparing memory management");
 }
 
+var kernelExecuted = false;
+
 function Boot() {
   try {
     console.log("booting up...");
     InitMemManagement();
-    mainKernel();
+    function getScriptName() {
+    let timer = setTimeout(function() {
+      if (!kernelExecuted) {
+        mainKernel();
+        kernelExecuted = true;
+      }
+    }, 800);
     return;
   } catch (e) {
     document.write("Error has occoured.<br>Error Message : " + e.Message + "<br>Stack : " + e.Stack);
